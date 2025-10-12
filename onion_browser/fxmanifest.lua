75c2be6ed33e476bdb3d57d14c485b64d711eb35
As2Trèfle <89260613+As2Trefle@users.noreply.github.com>
2025-10-12 18:32:10 +0200

    Add files via upload

---
+++ onion_browser/fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'onion_browser'
+author 'Dan'
+description 'LB-Phone app: Onion Browser (search engine)'
+version '0.1.0'
+lua54 'yes'
+
+files {
+  'ui/index.html',
+  'ui/styles/*.css',
+  'ui/scripts/*.js',
+  'ui/assets/*',
+  'ui/pages/**/**',
+
+}
+
+client_scripts {
+  'client.lua'
+}
